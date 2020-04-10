7c4725d45c952367f9871b99ad9a6607ff6a7a5d
RanolP <public.ranolp@gmail.com>
2020-04-10 19:37:16 +0900

    Solve 2422 on BOJ

---
+++ 2422/solution.rs
@@ -0,0 +1,148 @@
+//! This is free and unencumbered software released into the public domain.
+//!
+//! Anyone is free to copy, modify, publish, use, compile, sell, or
+//! distribute this software, either in source code form or as a compiled
+//! binary, for any purpose, commercial or non-commercial, and by any
+//! means.
+//!
+//! In jurisdictions that recognize copyright laws, the author or authors
+//! of this software dedicate any and all copyright interest in the
+//! software to the public domain. We make this dedication for the benefit
+//! of the public at large and to the detriment of our heirs and
+//! successors. We intend this dedication to be an overt act of
+//! relinquishment in perpetuity of all present and future rights to this
+//! software under copyright law.
+//!
+//! THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//! EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//! MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//! IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+//! OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+//! ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//! OTHER DEALINGS IN THE SOFTWARE.
+//!
+//! For more information, please refer to <http://unlicense.org/>
+//!
+//! Source Code URL:
+//! https://gist.github.com/RanolP/3d17fdb99fbf30d6c67bfac1cc366133
+
+use std::io::{stdin, BufRead, ErrorKind, StdinLock};
+
+fn get_word(stdin: &mut StdinLock) -> String {
+    let mut buffer = Vec::new();
+    loop {
+        let available = match stdin.fill_buf() {
+            Ok(n) => n.to_vec(),
+            Err(ref e) if e.kind() == ErrorKind::Interrupted => continue,
+            _ => panic!(),
+        };
+        if available.len() == 0 && buffer.len() != 0 {
+            break String::from_utf8(buffer).unwrap().trim().to_string();
+        }
+        let count = available
+            .iter()
+            .enumerate()
+            .find(|(_, chr)| match chr {
+                b' ' | b'\n' | b'\r' => true,
+                _ => false,
+            })
+            .map(|(idx, _)| idx);
+        if let Some(count) = count {
+            buffer.extend_from_slice(&available[..count]);
+            stdin.consume(count + 1);
+            if !buffer.is_empty() {
+                break String::from_utf8(buffer).unwrap().trim().to_string();
+            }
+        } else {
+            buffer.extend_from_slice(&available);
+            stdin.consume(available.len());
+        }
+    }
+}
+
+macro_rules! input {
+    (@inner $stdin:ident; line) => {{
+        let mut buf = String::new();
+        $stdin.read_line(&mut buf).unwrap();
+        buf.trim_end_matches('\n').to_string()
+    }};
+    (@inner $stdin:ident; word) => {
+        get_word(&mut $stdin)
+    };
+    (@inner $stdin:ident; ($($t:ty),*)) => {
+        ($(input!(@inner $stdin; $t),)*)
+    };
+    (@inner $stdin:ident; [($($t:ty),*); $size:literal]) => {{
+        let mut result = [<($($t),*) as Default>::default(); $size];
+        for i in 0..$size {
+            result[i] = ($(input!(@inner $stdin; $t)),*);
+        }
+        result
+    }};
+    (@inner $stdin:ident; [$size:literal; $t:ty]) => {
+        input!(@inner $stdin; [($t); $size])
+    };
+    (@inner $stdin:ident; Vec[$size:expr; $($t:tt)+]) => {{
+        let mut result = Vec::with_capacity($size);
+        for _ in 0..$size {
+            result.push(input!(@inner $stdin; $($t)*));
+        }
+        result
+    }};
+    (@inner $stdin:ident; HashSet[$size:expr; $($t:tt),+]) => {{
+        let mut result = std::collections::HashSet::with_capacity($size);
+        for _ in 0..$size {
+            result.insert(input!(@inner $stdin; $($t)*));
+        }
+        result
+    }};
+    (@inner $stdin:ident; $t:ty) => {
+        input!(@inner $stdin; word).parse::<$t>().unwrap()
+    };
+    ($size:literal; ($($t:ty),*)) => {
+        input!([$size; ($($t),*)])
+    };
+    ($size:literal; $t:ty) => {
+        input!([$t; $size])
+    };
+    ($($($t:tt)+),*) => {{
+        let stdin = stdin();
+        let mut stdin = stdin.lock();
+        ($(input!(@inner stdin; $($t)+)),*)
+    }};
+}
+
+use std::collections::HashSet;
+
+fn main() {
+    let count = input!(usize);
+    let do_not_mix_count = input!(usize);
+    let do_not_mix = input!(Vec[do_not_mix_count; [2; usize]]);
+
+    let mut do_not_mix_map = vec![HashSet::new(); count + 1]; // make it 1-indexed.
+
+    for [i, j] in do_not_mix {
+        let (min, max) = if i < j { (i, j) } else { (j, i) };
+        do_not_mix_map[min].insert(max);
+    }
+
+    let mut available = 0;
+
+    for i in 1..=count {
+        for j in (i + 1)..=count {
+            for k in (j + 1)..=count {
+                let set = &do_not_mix_map[i];
+                if set.contains(&j) || set.contains(&k) {
+                    continue;
+                }
+                let set = &do_not_mix_map[j];
+                if set.contains(&k) {
+                    continue;
+                }
+                available += 1;
+            }
+        }
+    }
+
+    println!("{}", available);
+}
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # @daily-boj/RanolP
 
+> 노트 시스템을 갈아엎기 전까지 노트는 일시적으로 제공하지 않고 있습니다.
+
 ## 문제 풀이 현황
 
 <table>
@@ -13,33 +15,39 @@
   <img src="https://static.solved.ac/tier_small/5.svg" height="16px"/>
   Bronze I, 1340 연도 진행바
 </a>
-</td><td><a href="./1340/solution.py">풀이 (.py)</a>, <a href="./1340/README.md">노트</a></td></tr>
+</td><td><a href="./1340/solution.py">풀이 (.py)</a></td></tr>
 <tr><td rowspan="1">2020-04-08</td><td>
 <a href="https://noj.am/10166">
   <img src="https://static.solved.ac/tier_small/9.svg" height="16px"/>
   Silver II, 10166 관중석
 </a>
-</td><td><a href="./10166/solution.py">풀이 (.py)</a>, <a href="./10166/README.md">노트</a></td></tr>
+</td><td><a href="./10166/solution.py">풀이 (.py)</a></td></tr>
 <tr><td rowspan="2">2020-04-09</td><td>
 <a href="https://noj.am/1935">
   <img src="https://static.solved.ac/tier_small/8.svg" height="16px"/>
   Silver III, 1935 후위 표기식2
 </a>
-</td><td><a href="./1935/solution.rs">풀이 (.rs)</a>, <a href="./1935/README.md">노트</a></td></tr>
+</td><td><a href="./1935/solution.rs">풀이 (.rs)</a></td></tr>
 <tr><td>
 <a href="https://noj.am/1735">
   <img src="https://static.solved.ac/tier_small/9.svg" height="16px"/>
   Silver II, 1735 분수 합
 </a>
 </td><td><a href="./1735/solution.py">풀이 (.py)</a></td></tr>
+<tr><td rowspan="1">2020-04-10</td><td>
+<a href="https://noj.am/2422">
+  <img src="https://static.solved.ac/tier_small/6.svg" height="16px"/>
+  Silver V, LV1 2422 한윤정이 이탈리아에 가서 아이스크림을 사먹는데
+</a>
+</td><td><a href="./2422/solution.rs">풀이 (.rs)</a></td></tr>
   </table>
 
 ## 언어 사용량
 
 | 언어 | 사용 비율 |
 | ---- | --------- |
-| Python (.py) | 3 of 4 (75%) |
-| Rust (.rs) | 1 of 4 (25%) |
+| Python (.py) | 3 of 5 (60%) |
+| Rust (.rs) | 2 of 5 (40%) |
 
 ## 연결고리
 
